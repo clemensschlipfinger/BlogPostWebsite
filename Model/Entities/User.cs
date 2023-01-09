c5390cc5b6736485a09359a750f30a06b914f4e6
Clemens Schlipfinger <clemens.schlipfinger@gmail.com>
2023-01-09 23:59:21 +0100

    added description to user

--- Model/Entities/User.cs
+++ Model/Entities/User.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Identity;
 
 namespace Model.Entities;
@@ -14,4 +15,8 @@ public class User : IdentityUser
     [Required, StringLength(100)]
     [Column("LASTNAME")]
     public string LastName { get; set; }
+
+    [Required]
+    [Column("DESCRIPTION"), DataType(DataType.Text)]
+    public string Description { get; set; } = "I love to write about interesting topics.";
 }
